7a76673f91e90804cebce26c82ef8da0d9bc780b
MD Mehedi Hasan Tuhin <30876490+TuhinLK@users.noreply.github.com>
2023-02-14 13:47:43 +0600

    Time: 95 ms (80.53%), Space: 212.9 MB (38.93%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.rb
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.rb
@@ -3,6 +3,5 @@
 # @return {Float}
 def find_median_sorted_arrays(nums1, nums2)
     nums = (nums1 + nums2).sort
-    length = nums.length
-    (nums[ (length - 1) / 2] + nums[length / 2]) / 2.0
+    (nums[ (nums.length - 1) / 2] + nums[nums.length / 2]) / 2.0
 end
