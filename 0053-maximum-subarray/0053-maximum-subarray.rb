b42368031a79c7df520c0e24c309b205f3307a43
MD Mehedi Hasan Tuhin <30876490+TuhinLK@users.noreply.github.com>
2023-02-15 02:38:27 +0600

    Time: 155 ms (91.40%), Space: 219.2 MB (44.62%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def max_sub_array(nums)
+    max_sum = nums[0]
+    initial = 0
+    nums.each { |num| max_sum = [max_sum , initial = [num, initial + num].max ].max }
+    max_sum
+end
