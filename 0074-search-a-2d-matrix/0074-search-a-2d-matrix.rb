06b43d699b36a97ae85efa8e8961a497d5540301
MD Mehedi Hasan Tuhin <30876490+TuhinLK@users.noreply.github.com>
2023-02-22 01:25:48 +0600

    Time: 84 ms (62.07%), Space: 211.3 MB (49.43%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rb
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rb
@@ -2,5 +2,18 @@
 # @param {Integer} target
 # @return {Boolean}
 def search_matrix(matrix, target)
-    matrix.flatten.include?(target)
+    # matrix.flatten.include?(target)
+    return false if matrix.nil?
+    m, n = matrix.length, matrix[0].length
+    i, j = 0, n - 1
+    while i < m && j >= 0
+        if matrix[i][j] == target
+            return true
+        elsif matrix[i][j] < target
+            i += 1
+        else
+            j -= 1
+        end
+    end
+    return false
 end
