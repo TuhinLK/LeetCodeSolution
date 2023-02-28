1a4d3d79b889b39abd35daea841216e209d45a17
MD Mehedi Hasan Tuhin <30876490+TuhinLK@users.noreply.github.com>
2023-03-01 01:54:18 +0600

    Time: 115 ms (39.13%), Space: 211.6 MB (8.70%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.rb
+++ 1929-concatenation-of-array/1929-concatenation-of-array.rb
@@ -1,5 +1,14 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def get_concatenation(nums)
-    [nums,nums].flatten
+    # [nums,nums].flatten
+    n = 1
+    ans = []
+    while n <= 2
+        nums.each do |i|
+            ans.push(i)
+        end
+        n +=1
+    end
+    ans
 end
